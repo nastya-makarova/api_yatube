c93ad7ea477dc8d9c2baa2e7f7bf11df2063799f
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-21 17:22:29 +0400

    Deleted has_permission() from custom permissiom; in get_query_set changed return from manger to queryset.

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,9 +4,6 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """Разрешение, позволяющее редактировать объект только авторам."""
 
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
-
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
@@ -13,7 +14,7 @@ from posts.models import Group, Post
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly, )
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -26,14 +27,14 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly, )
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
 
     def get_post(self):
         post_id = self.kwargs['post_id']
         return get_object_or_404(Post, pk=post_id)
 
     def get_queryset(self):
-        return self.get_post().comments
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, post=self.get_post())
